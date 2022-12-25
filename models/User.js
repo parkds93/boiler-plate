798aa9b7470d73b83713b125d8de585a2f9b020d
박대성 <8757279@naver.com>
2022-12-25 22:44:15 +0900

    22-12-25 토큰까지 생성했음

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const salRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,51 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    // 비밀번호 변경시에만
+    if(user.isModified("password")){
+        // 비밀번호 암호화
+        bcrypt.genSalt(salRounds, function( err, salt){
+            if(err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // plainPassword = 1234567
+    // 암호화된 비밀번호 = $2b$10$RBUl0/4O1zYE0gipsVA4k.QXJtWH5uymAlnln3ddm1OL.4DAdOTsm
+
+    bcrypt.compare(plainPassword, this.password ,function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    
+    // jsonwebtoken 을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        
+        cb(null, user);
+    })
+
+}
 
 const User = mongoose.model('User', userSchema)
 
